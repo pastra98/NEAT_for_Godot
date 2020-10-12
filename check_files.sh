9db94ede23c02d21ce20c2e13bfe8e943cc137bd
Paul Straberger <p.straberger@gmail.com>
2020-10-12 13:55:37 +0200

    check_files now works from root dir

--- check_files.sh
+++ check_files.sh
@@ -2,4 +2,4 @@
 # quick hack yto check all resources referenced anywhere are present on filesystem.
 # This is useful also to catch case-insensitive file names with wrong case (the application
 # works just fine on Windows, but will fail on linux)
-fgrep -r "res://" . | sed -e 's+^\([^:]*\):.*res://\([^\\"]*\)[\\"].*$+\1 \2+g' | while read f l; do [ -f "$l" ] || echo missing "$l" in "$f"; done
+fgrep -r "res://" src | sed -e 's+^\([^:]*\):.*res://\([^\\"]*\)[\\"].*$+\1 \2+g' | while read f l; do [ -f "$l" ] || echo missing "$l" in "$f"; done
