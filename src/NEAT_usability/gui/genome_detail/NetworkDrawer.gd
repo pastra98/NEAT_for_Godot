4b86b5f6dd3a604f9c81831a26cbc9809e4fe676
Paul Straberger <p.straberger@gmail.com>
2020-10-19 18:26:19 +0200

    wip, need to checkout branch

--- src/NEAT_usability/gui/genome_detail/NetworkDrawer.gd
+++ src/NEAT_usability/gui/genome_detail/NetworkDrawer.gd
@@ -4,40 +4,67 @@ extends Control
 """
 
 
-# the font used
 var roboto = load("res://NEAT_usability/fonts/dynamics/roboto-black.tres")
+var neuron_radius = 6
+var link_thickness = 3
+var max_offset = 150
+
+# that shit don't work? feed back working?
+# remember to add loop links
 
 func _draw():
     """Loops through every neuron and draws it along with it's input connections.
     """
     # get the required information from it's owner (a GenomeDetail)
-    var depth = owner.inspected_genome.agent.network.depth
     var neurons_dict = owner.inspected_genome.neurons
-    for neuron_id in neurons_dict.keys():
-        # determine the position of the neuron on the canvas and it's color
-        var neuron = neurons_dict[neuron_id]
-        var draw_pos = neuron.position * rect_size
-        var draw_col = Params.neuron_colors[neuron.neuron_type]
-        # first draw all links connecting to the neuron
-        for link in neuron.input_connections:
-            # color strength is determined by how strong weight relative to wmc
-            var w_col = Color(1, 1, 1, 1)
-            var wmc = Params.weight_max_color
-            var w_col_str = (wmc - min(abs(link[1]), wmc)) / wmc
-            # color red by decreasing green and blue
-            if link[1] >= 0:
-                w_col.g = w_col_str; w_col.b = w_col_str
-            # color blue by decreasing red and green
-            elif link[1] <= 0:
-                w_col.r = w_col_str; w_col.g = w_col_str
-            # draw links as tris to indicate their firing direction
-            var in_pos = link[0].position * rect_size
-            var spacing = Vector2(0, 5)
-            var tri_points = PoolVector2Array([in_pos+spacing, draw_pos, in_pos-spacing])
-            var colors = PoolColorArray([Color.white, w_col, Color.white])
-            draw_primitive(tri_points, colors, tri_points)
+    for neuron in neurons_dict.values():
+        # holds the angles of links leading from the input neuron. prevent overlapping
+        var connection_angles = []
+        for link_data in neuron.input_connections:
+            var from = neuron
+            var to = link_data[0]
+            var weight = link_data[1]
+            var connection_angle = Vector2(0,1).angle_to(to.position - from.position)
+            # offsets the connection parallel if it would overlap with another conn.
+            var connection_offset = 0
+            # check if the input neuron already has a connection with the same angle
+            if connection_angles.has(connection_angle):
+                connection_offset = from.position.x - to.position.x * max_offset
+            else:
+                connection_angles.append(connection_angle)
+            #
+            draw_link(from, to, weight, connection_offset, connection_angle)
+################################################################################
         # finally draw the neuron last, so it overlaps all the links
-        draw_circle(draw_pos, 6, draw_col)
+        var draw_col = Params.neuron_colors[neuron.neuron_type]
+        draw_circle(neuron.position * rect_size, 6, draw_col)
         # mark if a loop link is connected to the neuron
         if neuron.loop_back:
-            draw_char(roboto, draw_pos, "L", "", Color.black)
+            draw_char(roboto, neuron.position * rect_size, "L", "", Color.black)
+################################################################################
+
+
+func draw_link(from: Neuron, to: Neuron, weight: float, offset: float, angle: float) -> void:
+    """
+    """
+    # the points on the drawing canvas
+    var from_pos = from.position * rect_size
+    var to_pos = to.position * rect_size
+    # link color strength is determined by how strong the weight  is relative to wmc
+    var w_col = Color(1, 1, 1, 1)
+    var wmc = Params.weight_max_color
+    var w_col_str = (wmc - min(abs(weight), wmc)) / wmc
+    # color red by decreasing green and blue
+    if weight >= 0:
+        w_col.g = w_col_str; w_col.b = w_col_str
+    # color blue by decreasing red and green
+    elif weight <= 0:
+        w_col.r = w_col_str; w_col.g = w_col_str
+    # draw the link
+    if abs(offset) > 0:
+        var angle1 = angle + PI/2 if offset > 0 else angle - PI/2
+        var angle2 = -angle1
+        draw_line(from_pos, from_pos + Vector2(0,offset).rotated(angle1), w_col, link_thickness)
+        draw_line(from_pos + Vector2(0,offset).rotated(angle1), to_pos, w_col, link_thickness)
+    else:
+        draw_line(from_pos, to_pos, w_col, link_thickness)
