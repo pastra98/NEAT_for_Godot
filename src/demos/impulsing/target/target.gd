a70fadee8608291a5ed9145567b00b6519b3475a
Paul Straberger <p.straberger@gmail.com>
2022-10-10 16:00:33 +0200

    now rotates along circle in fraction

--- src/demos/impulsing/target/target.gd
+++ src/demos/impulsing/target/target.gd
@@ -8,16 +8,23 @@ func _ready():
     global_position = Vector2(distance_to_spawn, 0)
 
 
-func move_to_new_pos(flip_side: bool):
+func move_to_new_random_pos(flip_side: bool):
     if flip_side:
         global_position = global_position.rotated(PI)
     else:
         global_position = global_position.rotated(Utils.random_f_range(-PI/4, PI/4))
 
 
+func move_by_fraction(fraction: int):
+    global_position = global_position.rotated(TAU/fraction)
+    
+
+
 func _unhandled_input(event):
-    if event is InputEventKey and event.scancode == KEY_SPACE:
-        move_to_new_pos(true)
+    # use r to move target to new random pos
+    if event is InputEventKey and event.scancode == KEY_R:
+        move_to_new_random_pos(true)
+    # drag target with mouse
     elif event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
         dragging = true
         if dragging and !event.pressed:
